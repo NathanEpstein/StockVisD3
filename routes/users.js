e87c5dd41c489cc14af71f5fa275cfc4c4ad812c
nepstein <ne2210@columbia.edu>
2014-09-30 09:13:34 -0400

    commence d3 timeline

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
